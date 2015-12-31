9232a74149cee633ab166ad0a7eb23c7484c6491
zohartz <tz.zohar@gmail.com>
2015-12-31 16:07:23 +0200

    Initial commit

---
+++ app/src/main/java/mygui/com/zohar/mytodolist/ForgotPassword.java
@@ -0,0 +1,21 @@
+package mygui.com.zohar.mytodolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        return true;
+    }
+
+}
